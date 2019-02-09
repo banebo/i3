dd4ac590550eab40cba48f3eb914fbe04df14968
banebo <branislavbozejac@gmail.com>
2019-02-09 18:29:38 +0100

    Fixes

--- setup.sh
+++ setup.sh
@@ -1,17 +1,23 @@
 #!/usr/bin/env bash
 
 echo "[*] Checking i3 installation..."
-type i3 > /dev/null
+type i3 i3status > /dev/null
 if [[ $? -ne 0 ]]; then
     read -p "[-] i3 is missing, would you like to install it [y/n]? " choice
     if [[ $choice = "y" ]]; then
-        sudo apt-get install i3 -y
+        type apt-get > /dev/null
+        if [[ $? -eq 0 ]]; then
+            sudo apt-get install i3 -y
+        else
+            sudo pacman -S i3-wm
+            sudo pacman -S i3status
         echo "[+] Done"
+        fi
     else
         echo "[-] Aborting..."
         exit 1
     fi
-    
+
 else
     echo "  [+] OK"
 fi
@@ -19,7 +25,7 @@ fi
 if [[ ! -d $HOME/.config/i3 ]]; then
     echo "[*] Running i3-config-wizard..."
     i3-config-wizard
-    echo "  [+] Done"  
+    echo "  [+] Done"
 fi
 
 echo
